8a06308ee2be5ab55b46f58b5cdc21fbf6f5ccb1
UserPasha <pasharoma13@gmail.com>
2022-11-24 22:03:03 +0300

    draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import "./App.css"
 
 
 
 function App() {
   return (
-   <>Hello</>
+      <div className={"appWrapper"}>
+        <div className={"header"}>
+            Blogger Platform
+        </div>
+        <div className={"navbar"}>
+            <div>Blogs</div>
+            <div>Posts</div>
+        </div>
+        <div className={"content"}>
+            <div>Blogs</div>
+            <div>Search+Filter</div>
+            <div>MappingPosts</div>
+        </div>
+      </div>
+
   );
 }
 
